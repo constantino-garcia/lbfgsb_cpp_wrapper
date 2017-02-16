98289abc37dea75aa8c5b10426d4673fef877df4
Constantino Antonio García Martínez <constantinoantonio.garcia@usc.es>
2017-02-16 13:30:47 +0100

    Add example using Eigen3 vectors

--- l_bfgs_b_example.cpp
+++ l_bfgs_b_example.cpp
@@ -2,6 +2,7 @@
 #include <armadillo>
 #include "tests/src/test_functions.h"
 #include "l_bfgs_b.h"
+#include <Eigen/Dense>
 
 using namespace std;
 
@@ -14,7 +15,11 @@ int main() {
     l_bfgs_b<std::vector<double> > stdSolver(5);
     stdSolver.setVerboseLevel(-1);
 
-    std::cout << "*** MINIMIZING GOLDSTEIN ***" << std::endl;
+    l_bfgs_b<Eigen::VectorXd> eigenSolver(5);
+    eigenSolver.setVerboseLevel(-1);
+    Eigen::VectorXd eigenX(2), lb(2), ub(2);
+
+    std::cout << "*** MINIMIZING GOLDSTEIN (with armadillo) ***" << std::endl;
     goldstein_price_function< arma::vec > gold;
     gold.set_lower_bound({-2, -2});
     gold.set_upper_bound({2, 2});
@@ -22,7 +27,7 @@ int main() {
     armaSolver.optimize(gold, armaX);
     std::cout << "MINIMUM LOCATED AT : (" << armaX[0] << ", " << armaX[1] << ")" << std::endl << std::endl;
 
-    std::cout << "*** MINIMIZING BOOTH ***" << std::endl;
+    std::cout << "*** MINIMIZING BOOTH (with std::vector) ***" << std::endl;
     booth_function<std::vector<double> > bl;
     bl.set_lower_bound({-2, -2});
     bl.set_upper_bound({5, 4});
@@ -30,13 +35,15 @@ int main() {
     stdSolver.optimize(bl, stdX);
     std::cout << "MINIMUM LOCATED AT : (" << stdX[0] << ", " << stdX[1] << ")" << std::endl << std::endl;
 
-    std::cout << "*** MINIMIZING MATYAS ***" << std::endl;
-    matyas_function< arma::vec > matyas;
-    matyas.set_lower_bound({-10, -10});
-    matyas.set_upper_bound({10, 10});
-    armaX = {8, -8};
-    armaSolver.optimize(matyas, armaX);
-    std::cout << "MINIMUM LOCATED AT : (" << armaX[0] << ", " << armaX[1] << ")" << std::endl << std::endl;
+    std::cout << "*** MINIMIZING MATYAS (with Eigen3) ***" << std::endl;
+    matyas_function< Eigen::VectorXd > matyas;
+    lb << -10, -10;
+    ub << 10, 10;
+    matyas.set_lower_bound(lb);
+    matyas.set_upper_bound(ub);
+    eigenX << 8, -8;
+    eigenSolver.optimize(matyas, eigenX);
+    std::cout << "MINIMUM LOCATED AT : (" << eigenX[0] << ", " << eigenX[1] << ")" << std::endl << std::endl;
 
     std::cout << "*** MINIMIZING BEALE ***" << std::endl;
     beale_function<std::vector<double> > beale;
